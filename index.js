cc574377c7f411054207fcc8e2c8267b9a5afb17
Peter Lemiszki <plemiszki@gmail.com>
2018-01-19 10:52:47 -0500

    new methods

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ var moment = require('moment');
 
 module.exports = {
 
+  alphabetizeArrayOfObjects: function(array, property) {
+    return array.sort(function(a, b) {
+      if (a[property].toUpperCase() < b[property].toUpperCase()) {
+        return -1;
+      } else if (a[property].toUpperCase() > b[property].toUpperCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  },
+
+  capitalize: function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+
+  pluralize: function(string, n) {
+    if (n === 1) {
+      return this;
+    } else {
+      return this + 's';
+    }
+  },
+
+  removeFinanceSymbols: function(string) {
+    return string.replace('$', '').replace(',', '');
+  },
+
   removeFromArray: function(array, element) {
     var index = array.indexOf(element);
     if (index >= 0) {
@@ -21,6 +49,18 @@ module.exports = {
       if (+moment(a[property]).format('x') < +moment(b[property]).format('x')) {
         return -1;
       } else if (+moment(a[property]).format('x') > +moment(b[property]).format('x')) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  },
+
+  sortArrayOfObjects: function(array, property) {
+    return array.sort(function(a, b) {
+      if (parseInt(a[property]) < parseInt(b[property])) {
+        return -1;
+      } else if (parseInt(a[property]) > parseInt(b[property])) {
         return 1;
       } else {
         return 0;
