c765c924fec851b924ffaefed9cb708935f816d0
Peter Lemiszki <plemiszki@gmail.com>
2019-01-16 13:32:26 -0500

    sortArrayOfObjects method can take two properties

--- index.js
+++ index.js
@@ -398,14 +398,26 @@ module.exports = {
     });
   },
 
-  sortArrayOfObjects: function(array, property) {
+  sortArrayOfObjects: function(array, arg) {
+    let property = Array.isArray(arg) ? arg[0] : arg;
     return array.sort(function(a, b) {
       if (parseInt(a[property]) < parseInt(b[property])) {
         return -1;
       } else if (parseInt(a[property]) > parseInt(b[property])) {
         return 1;
       } else {
-        return 0;
+        if (Array.isArray(arg)) {
+          property = arg[1];
+          if (parseInt(a[property]) < parseInt(b[property])) {
+            return -1;
+          } else if (parseInt(a[property]) > parseInt(b[property])) {
+            return 1;
+          } else {
+            return 0;
+          }
+        } else {
+          return 0;
+        }
       }
     });
   },
