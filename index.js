5bdb1cac686f823ef0b60c4e0960e0b5ea039a10
Peter Lemiszki <plemiszki@gmail.com>
2021-03-15 11:00:36 -0400

    add stringIsNumber, stringIsDate

--- index.js
+++ index.js
@@ -293,6 +293,32 @@ module.exports = {
     return year + '-' + month + '-' + day;
   },
 
+  stringIsDate(string) {
+    if (!/^\d{1,2}\/\d{1,2}\/\d{2}$/.test(string)) { // First check for the pattern
+      return false;
+    }
+
+    const parts = string.split('/');
+    const day = parseInt(parts[1], 10);
+    const month = parseInt(parts[0], 10);
+    const year = parseInt(parts[2], 10);
+
+    if (month == 0 || month > 12) { // Check the range of month
+      return false;
+    }
+
+    const monthLengths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) { // Adjust for leap years
+      monthLengths[1] = 29;
+    }
+
+    return day > 0 && day <= monthLengths[month - 1]; // Check the range of the day
+  },
+
+  stringIsNumber(string) {
+    return !isNaN(string) && !isNaN(parseFloat(string));
+  },
+
   sortArrayOfDateStrings: function(array, property) {
     return array.sort(function(a, b) {
       if (+moment(a[property]).format('x') < +moment(b[property]).format('x')) {
