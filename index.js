10294a7a262d31624b41532c37b31d944a2c107e
Peter Lemiszki <plemiszki@gmail.com>
2022-01-11 08:37:57 -0500

    alphabetize objects - add error messages

--- index.js
+++ index.js
@@ -20,7 +20,16 @@ module.exports = {
   },
 
   alphabetizeArrayOfObjects: function(array, property) {
+    if (array === undefined) {
+      throw `alphabetize array of objects - array is missing (alphabetizing by ${property})`;
+    }
+    if (property === undefined) {
+      throw `alphabetize array of objects - property is missing`;
+    }
     return array.sort(function(a, b) {
+      if (a[property] === undefined || b[property] === undefined) {
+        throw `alphabetize array of objects - ${property} property does not exist in array element`;
+      }
       if (a[property].toString().toUpperCase() < b[property].toString().toUpperCase()) {
         return -1;
       } else if (a[property].toString().toUpperCase() > b[property].toString().toUpperCase()) {
